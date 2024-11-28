8375222a2db6960404a86febb5f9ec191de3fbc2
SeekerDev <albertbaisa@gmail.com>
2024-11-28 14:07:55 +0800

    commit

--- app/dashboard/todo/page.tsx
+++ app/dashboard/todo/page.tsx
@@ -7,11 +7,11 @@ export default function Todo() {
 	return (
 		<div className="space-y-5 w-full overflow-y-auto px-3">
 			<h1 className="text-3xl font-bold">Todo</h1>
-			<div className="flex gap-2">
+			{/* <div className="flex gap-2">
 				<SearchTodo />
 				<CreateTodo />
 			</div>
-			<MemberTable />
+			<MemberTable /> */}
 		</div>
 	);
 }
